847bc04f25cc4a4cd1673d87a44daabdc08d852b
San <miltus31@gmail.com>
2020-12-04 23:26:09 +0530

    fixed method2

--- method2.py
+++ method2.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 import torch
 
-from model import BGRestoreNet
+from model import TextExtractor, BGRestoreNet
 
 
 def merge_patches(accum, count, patches_data, idxs):
@@ -25,7 +25,7 @@ def merge_patches(accum, count, patches_data, idxs):
         count[i:i+patch_r, j:j+patch_c] += 1
 
 
-def patch_process_mini_batch(model, in_im, patch_size, stride,
+def patch_process_mini_batch(in_im, model, patch_size, stride,
                              batch_size, device, debug=True):
     '''
     Apply model to in_im in mini batches
@@ -110,14 +110,17 @@ def patch_process_mini_batch(model, in_im, patch_size, stride,
     # this can only occur in the right and bottom
     patch_end_r = row_v[-1]
     patch_end_c = col_v[-1]
+    patch_batch = np.zeros((1, nch) + patch_size, dtype='float32')
+
     if patch_end_r + patch_r < nrow-1:
         # there are pixels at bottom that are not covered
         for i in range(ncol-patch_c+1):
             r = nrow-patch_r
             c = i
             patch = im[:, r:r+patch_r, c:c+patch_c]
+            patch_batch[0] = patch
 
-            patch = torch.from_numpy(patch[None, ...]).to(device)
+            patch = torch.from_numpy(patch_batch).to(device)
             with torch.no_grad():
                 out_patch = model(patch)
 
@@ -133,10 +136,10 @@ def patch_process_mini_batch(model, in_im, patch_size, stride,
         for i in range(nrow-patch_r+1):
             r = i
             c = ncol-patch_c
-
             patch = im[:, r:r+patch_r, c:c+patch_c]
+            patch_batch[0] = patch
 
-            patch = torch.from_numpy(patch[None, ...]).to(device)
+            patch = torch.from_numpy(patch_batch).to(device)
             with torch.no_grad():
                 out_patch = model(patch)
 
@@ -156,9 +159,53 @@ def patch_process_mini_batch(model, in_im, patch_size, stride,
     return out_im
 
 
+def restore_image(in_im_PIL, text_extractor_net, bg_restore_net, patch_size,
+                  stride, device, inference_batch_size):
+
+    if in_im_PIL.mode != 'RGB':
+        in_im_PIL = in_im_PIL.convert('RGB')
+
+    in_im_uint8 = np.asarray(in_im_PIL)
+    in_im = in_im_uint8/255.0
+    in_im_gray = np.array(in_im_PIL.convert('L'))/255.0
+
+    in_im_gray = np.expand_dims(in_im_gray, -1)
+    # The trained CNN requires a grayscale image
+
+    # main method
+    # # extract text
+    text_im = patch_process_mini_batch(in_im_gray, text_extractor_net,
+                                       patch_size, stride,
+                                       inference_batch_size, device)
+
+    # Threshold selection
+    text_im_uint8 = (text_im*255).astype('uint8')
+    # bincount requires 1D array
+    hist = np.bincount(text_im_uint8.flatten(), minlength=256)
+    smoothed_hist = np.convolve(hist, np.ones((5, ))/5, mode='valid')
+    thr = np.argmin(smoothed_hist)+2
+    text_mask = text_im_uint8 < thr
+
+    # get background
+    bg = patch_process_mini_batch(in_im, bg_restore_net, patch_size, stride,
+                                  inference_batch_size, device)
+
+    # overlay
+    # foreground color restoration
+    fg = in_im*text_mask
+
+    # both fg and bg_smooth are uint8
+    out_im = fg + (1-text_mask)*bg
+
+    out_im_PIL = Image.fromarray(np.squeeze(out_im*255).astype('uint8'))
+
+    return out_im_PIL
+
+
 if __name__ == '__main__':
     # param
-    model_wt = './model/bgm1-960-0.13053849339485168.pt'
+    bg_model_wt = './model/bgm1-960-0.13053849339485168.pt'
+    text_model_wt = './model/upto2017_model_ourdata.pt'
 
     patch_size = (256, 256)
     stride = (10, 10)
@@ -182,22 +229,18 @@ if __name__ == '__main__':
 
     # inference on cpu
     device = torch.device('cpu')
-    model = BGRestoreNet()
-    model.load_state_dict(torch.load(model_wt, map_location=device))
-    model.eval()
+
+    text_model = TextExtractor()
+    text_model.load_state_dict(torch.load(text_model_wt, map_location=device))
+    text_model.eval()
+
+    bg_model = BGRestoreNet()
+    bg_model.load_state_dict(torch.load(bg_model_wt, map_location=device))
+    bg_model.eval()
 
     in_im_PIL = Image.open(input_im_path)
 
-    if in_im_PIL.mode != 'RGB':
-        in_im_PIL = in_im_PIL.convert('RGB')
+    out_im_PIL = restore_image(in_im_PIL, text_model, bg_model, patch_size,
+                               stride, device, inference_batch_size)
 
-    # test the bg net
-    # for the time being just call the mini batch function
-    in_im_uint8 = np.asarray(in_im_PIL)
-    in_im = in_im_uint8/255.0
-
-    out_im = patch_process_mini_batch(model, in_im, patch_size, stride,
-                                      inference_batch_size, device)
-
-    out_im_PIL = Image.fromarray(np.squeeze(out_im*255).astype('uint8'))
     out_im_PIL.save(output_im_path)
